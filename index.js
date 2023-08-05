f0e1f692a9a88dde9202b3f6053ab06b799dad79
Mr-Johny-Blaska <121683366+jan-blaska@users.noreply.github.com>
2023-08-06 00:08:18 +0200

    24 minutes

--- index.js
+++ index.js
@@ -5,22 +5,47 @@ const scene = new THREE.Scene();
 
 // Create our sphere
 const geometry = new THREE.SphereGeometry( 3, 64, 64 ); 
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff83 } ); 
+const material = new THREE.MeshStandardMaterial( { color: 0x00ff83 } ); 
 const sphere = new THREE.Mesh( geometry, material ); 
 scene.add( sphere );
 
+// Sizes
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+
 // Light
-const light = new THREE.PointLight(0xffffff, 1, 100 );
-light.position.set(0, 10, 10);
+const light = new THREE.AmbientLight(0xffffff, 1, 100);
+light.position.set( 0, 10, 10 );
 scene.add( light );
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
-camera.position.z = 10;
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
+camera.position.z = 20;
 scene.add( camera );
 
 // Renderer
 const canvas = document.querySelector('#webgl');
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render( scene, camera );
+
+
+// Resize
+window.addEventListener('resize', function() {
+    // Update Sizes
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+    // Update Camera
+    camera.updateProjectionMatrix();
+    camera.aspect = sizes.width / sizes.height;
+    renderer.setSize(sizes.width, sizes.height);
+})
+
+const loop = () => {
+    renderer.render(scene, camera)
+    window.requestAnimationFrame(loop);
+}
+loop();
